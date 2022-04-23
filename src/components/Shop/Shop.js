6bb39d655321182fe4a8394dbb9d178271933285
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-04-23 22:25:38 +0600

    shop component

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <div className='shop'>
+           <div className='products-container'>
+               <h2>This is a shop</h2>
+           </div> 
+            <div className='Cart-container'>
+                <h2>Shopping Cart</h2>
+            </div> 
+        </div>
+    );
+};
+
+export default Shop;
