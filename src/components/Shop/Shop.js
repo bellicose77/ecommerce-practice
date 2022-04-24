4af2ba2368dd60cef39bc978a87fd37928dd089b
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-04-24 22:41:14 +0600

    total value done

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -5,7 +5,7 @@ import './Shop.css'
 
 const Shop = () => {
     const[products,setProducts]=useState([]);
-    const[cart,setCart]=useState([])
+    const[cart,setCart]=useState([]);
     useEffect(()=>{
         fetch('products.JSON')
         .then(res =>res.json())
