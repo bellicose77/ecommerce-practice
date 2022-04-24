e71918a4072f49be67a407e266d710084ea03511
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-04-24 22:02:03 +0600

    total value done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 const Cart = ({cart}) => {
+    let total = 0;
+    for(const product of cart){
+        total+=product.price;
+    }
     return (
         <div>
             <h2>Order Summary</h2>
             <p>Order items:{cart.length}</p>
-            <p>Total: </p>
+            <p>Total:{total} </p>
         </div>
     );
 };
